87a939cda2c74a81fb5fd260faffa38ad8230867
Mariya369 <mariyadotkova555@gmail.com>
2022-04-25 23:52:29 +0200

    Made #unit work

--- src/app.js
+++ src/app.js
@@ -73,15 +73,13 @@ function displaycelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let celsiusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitLinkTemperature);
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusLinkTemperature);
+celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 search("New York");
