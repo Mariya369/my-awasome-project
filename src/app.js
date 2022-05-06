9d6d28a9233e31e08d40b98f2c93890f2afab984
Mariya369 <mariyadotkova555@gmail.com>
2022-05-06 22:31:19 +0200

    Removed the °F imperial conversion

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ let forecast = response.data.daily;
     forecastHTML =
       forecastHTML +
       `
-      <div class="col-2">
+      <div class = "col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         
         <img
@@ -113,32 +113,9 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("#active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displaycelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("#active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displaycelsiusTemperature);
 
 search("New York");
